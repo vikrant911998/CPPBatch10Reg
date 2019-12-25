a2b5f0ee50fa8612581c0c0a735c51ea1e5ae7f6
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-12-25 11:11:21 +0530

    Add files via upload

---
+++ 25Dec2019/demo1.cpp
@@ -0,0 +1,16 @@
+// Loops->While
+
+#include<iostream>
+using namespace std;
+
+int main(){ 
+
+    int i=5; //initialization.
+
+    while(i>=1){ //condition
+        cout<<i<<endl;
+        i--; // increment or decrement.
+    }
+
+    return 0;
+}
---
+++ 25Dec2019/demo2.cpp
@@ -0,0 +1,18 @@
+// Loops->Do-While
+
+#include<iostream>
+using namespace std;
+
+int main(){ 
+
+    int i=5; //initialization.
+
+    do{
+
+        cout<<i<<endl;
+        i--; // increment or decrement.
+        
+    }while(i>=1); //condition
+
+    return 0;
+}
